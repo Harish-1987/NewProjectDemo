b7d974f721a4fba6721e09bd4987520b436cff19
Harish <harish.sukant8@gmail.com>
2024-01-07 16:51:34 +0530

    Modified Advance Messiging service 1.0

--- src/main/java/com/Study/Service/Impl/AdvancedMessageService.java
+++ src/main/java/com/Study/Service/Impl/AdvancedMessageService.java
@@ -3,7 +3,7 @@ package com.Study.Service.Impl;
 import com.Study.Service.MessageService;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-//It is a Advance messiging service.....
+//It is a Advance messiging service and coming with AI powered smart features.
 @Service
 public class sprinAdvancedMessageService implements MessageService {
 
