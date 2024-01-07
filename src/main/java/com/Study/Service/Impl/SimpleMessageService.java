bf72a3aa60fa37d8df20e16262c217aef06e265e
Harish <harish.sukant8@gmail.com>
2024-01-07 18:23:41 +0530

    Adding new branch

--- src/main/java/com/Study/Service/Impl/SimpleMessageService.java
+++ src/main/java/com/Study/Service/Impl/SimpleMessageService.java
@@ -5,7 +5,7 @@ import com.Study.Service.MessageService;
 import jakarta.persistence.Cacheable;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
+//Simple message service modified in feature branch
 
 @Service
 public class SimpleMessageService implements MessageService {
