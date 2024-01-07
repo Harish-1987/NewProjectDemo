c30da25f95be5e6745b382aa587426971a3eb8e7
Harish Kumar <136085737+Harish-1987@users.noreply.github.com>
2024-01-07 19:03:49 +0530

    Update MessageProcessor.java

--- src/main/java/com/Study/Service/Impl/MessageProcessor.java
+++ src/main/java/com/Study/Service/Impl/MessageProcessor.java
@@ -5,7 +5,7 @@ import com.Study.Service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
+//Comment
 @Service
 public class MessageProcessor {
 
